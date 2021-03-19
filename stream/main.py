fb575470541c92aa1e6a854b1b0ea8fb75035271
Adrian Darian <adarian@ucmerced.edu>
2021-03-18 20:43:44 -0700

    add: stream to read in station data

---
+++ stream/main.py
@@ -0,0 +1,170 @@
+import json
+import os
+import asyncio
+import requests
+import _thread as thread
+import sys
+import time
+import datetime
+from decouple import config
+
+from lib.genStationArray import genStationArray
+from requests.auth import HTTPBasicAuth
+from concurrent.futures import ThreadPoolExecutor, as_completed, process
+
+URL = config("URL")
+SWEEP_API_USER = config("SWEEP_API_ID")
+SWEEP_API_TOKEN = config("SWEEP_API_TOKEN")
+ENDPOINT = "wss://rt.sweepapi.com/?auth_user={0}&auth_key={1}".format(
+    SWEEP_API_USER, SWEEP_API_TOKEN)
+
+executor = ThreadPoolExecutor(max_workers=50)
+
+
+def getAccount():
+    return requests.get("{0}/account".format(URL), auth=HTTPBasicAuth(SWEEP_API_USER, SWEEP_API_TOKEN)).json()
+
+
+def getDirectory(directory_id):
+    return requests.get("{0}/directory/{1}".format(URL, directory_id), auth=HTTPBasicAuth(SWEEP_API_USER, SWEEP_API_TOKEN)).json()
+
+
+def getStream(stream_id):
+    return requests.get("{0}/stream/{1}".format(URL, stream_id), auth=HTTPBasicAuth(SWEEP_API_USER, SWEEP_API_TOKEN)).json()
+
+
+def genFolder(name, top_directory):
+    return requests.post("{0}/directory".format(URL), json={"name": name, "dirtop": top_directory}, auth=HTTPBasicAuth(SWEEP_API_USER, SWEEP_API_TOKEN)).json()
+
+
+def genStream(name, top_directory):
+    return requests.post("{0}/stream".format(URL), json={"name": name, "dirtop": top_directory, "inputDataVar": []}, auth=HTTPBasicAuth(SWEEP_API_USER, SWEEP_API_TOKEN)).json()
+
+
+def genTSParam(dataset, stream_id):
+    return requests.post("{0}/stream/{1}/ts".format(URL, stream_id), json=dataset, auth=HTTPBasicAuth(SWEEP_API_USER, SWEEP_API_TOKEN)).json()
+
+
+def saveComplete(key, home_store_directory):
+    station_folder[key] = {}
+    stream_item = [
+        item for item in home_store_directory["stream"] if item["name"] == key]
+    print(stream_item)
+
+    if len(stream_item):
+        print("Stream Exists: Will not Make a New Stream")
+        station_folder[key]["stream_id"] = stream_item[0]["stream_id"]
+    else:
+        print("New Stream will be created from JSON list")
+        response_stream = genStream(key, home_store_directory["directory_id"])
+        if response_stream["status"] == "ok":
+            station_folder[key]["stream_id"] = response_stream["stream_id"]
+
+    # this checks to see if ind station stream has wind_speed as ts_param
+    ind_station_stream = getStream(station_folder[key]["stream_id"])
+    var_name_select = "wind_speed"
+    stream_item_ts_param = [
+        item for item in ind_station_stream['schema'] if item["var_name"] == var_name_select]
+
+    if len(stream_item_ts_param):
+        station_folder[key]["ts_param"] = var_name_select
+    else:
+        response_stream_ts = genTSParam({"var_name": var_name_select, "display_name": "Wind Speed",
+                                         "description": "Realtime Wind Speed", "units": "mph", "type": "number"}, station_folder[key]["stream_id"])
+        if response_stream_ts["status"] == "ok":
+            station_folder[key]["ts_param"] = var_name_select
+
+
+def saveData(line):
+    return requests.post(line["url"], data=line["payload"], auth=HTTPBasicAuth(SWEEP_API_USER, SWEEP_API_TOKEN)).json()
+
+
+def sleepResolveError(error):
+    print("\rWaiting for error to pass" + str(error), end="")
+    time.sleep(10)
+
+
+if __name__ == "__main__":
+    home_directory = getDirectory("home")
+    check_station_directory = False
+    station_directory_name = "Sensors"
+    station_directory_id = ""
+    station_folder = {}
+    print(home_directory, flush=True)
+    directory_item_home_station_unique = [
+        item for item in home_directory["directory"] if item["name"] == station_directory_name]
+    print(len(directory_item_home_station_unique))
+
+    if len(directory_item_home_station_unique):
+        print("directory 'Sensors' exists")
+        station_directory_id = directory_item_home_station_unique[0]["id"]
+        station_directory = getDirectory(station_directory_id)
+        station_name = "Weather Stations"
+        station_ind_folder = [
+            item for item in station_directory["directory"] if item["name"] == station_name]
+
+        if len(station_ind_folder):
+            station_ind_directory_id = station_ind_folder[0]["id"]
+        else:
+            station_ind_gen_folder = genFolder(
+                station_name, station_directory_id)
+            print(station_ind_gen_folder)
+
+            if station_ind_gen_folder["status"] == "ok":
+                station_ind_directory_id = station_ind_gen_folder["id"]
+
+        station_ind_directory = getDirectory(station_ind_directory_id)
+        alias_station_stream_id = {}
+
+        for line_data in station_ind_directory["stream"]:
+            stream_id = line_data["name"]
+            alias_station_stream_id[stream_id] = line_data
+
+        # update stream portion of dict to reflect easy
+        array_data = ["data/stations.json"]
+        stations_list = genStationArray(array_data).getStations()
+        processes = {}
+
+        for key in stations_list:
+            key = key.strip()
+            job = executor.submit(saveComplete, key, station_ind_directory)
+            processes[job] = key
+
+        while len(processes) > 0:
+            time.sleep(1)
+            for x in as_completed(processes):
+                del processes[x]
+
+        print("Stage 1: Complete", flush=True)
+        s_list = stations_list.keys()
+
+        while True:
+            url_dataset = []
+            # Once all streams and directories defined. Make request for data and send back to API
+            time_now = datetime.datetime.utcnow().replace(microsecond=0).isoformat()
+
+            for station_item in s_list:
+                print("Saving Raw Sample Data for {} ".format(station_item))
+
+                if station_item is not None:
+                    line = {}
+                    sample = 34.5  # Randome data
+                    line["payload"] = {"timestamp": time_now, "sample": sample}
+                    line["url"] = "/stream/{}/ts/{}/dataset".format(
+                        URL, alias_station_stream_id[station_item]["id"], "wind_speed")
+                    url_dataset.append(line)
+                    print(url_dataset)
+
+            # Makes http request in parallel
+            for urldata in url_dataset:
+                job = executor.submit(saveData, urldata)
+                processes[job] = urldata
+
+            # verify if job complete and del completed instances
+            for x in as_completed(processes):
+                del processes[x]
+
+            print("Regular loop for collection data")
+            time.sleep(5)
+    else:
+        print("'Sensors' Directory Does Not exists")
--- sweep_api_examples/testWS.js
+++ sweep_api_examples/testWS.js
@@ -1,7 +1,7 @@
 var ws = require("ws");
 // AUTH INFO
-auth_user_id = ""; /// user key from api_keys
-auth_token = ""; // token from api_keys
+auth_user_id = "263bec6f-d00b-4c18-998a-b868bfdcee1a"; /// user key from api_keys
+auth_token = "2653b7ec-578e-4836-a956-01d0a51af78b"; // token from api_keys
 
 endpoint = "wss://rt.sweepapi.com";
 
@@ -26,9 +26,14 @@ socket.onmessage = function (event) {
 			safe_to_send = 1;
 			console.log("Starting Sweep WS Realtime Session");
 
-			// CREATE A WATCH STREAM Requests
-			// const msg = {"ws_action": "watch", "watch_streams":["c2ee0061-1b7e-4311-968c-083e1c0d672b"]}
-			// socket.send(JSON.stringify(msg));
+			// START : CREATE A WATCH STREAM Requests
+			const msg = {
+				ws_action: "watch",
+				watch_streams: ["3abeb1e3-9a77-47bc-ba1d-18fe9812f8c0"],
+			};
+			socket.send(JSON.stringify(msg));
+			console.log("Creating a Watch Stream");
+			// END : CREATE A WATCH STREAM Requests
 
 			// TEST POST Request when using REALTIME API
 
@@ -46,14 +51,14 @@ socket.onmessage = function (event) {
 
 			// TEST GET Requests when using REALTIME API
 
-			// // START : GET HOME DIRECTORY
-			// const TEST_MESSAGE = {}
-			// TEST_MESSAGE["action_url"] = "/directory/home";
-			// TEST_MESSAGE["http_type"] = "GET";
-			// TEST_MESSAGE["unique_query_id"] = "12345";
-			// socket.send(JSON.stringify(TEST_MESSAGE));
-			// console.log("Sending GET message")
-			// // END : GET HOME DIRECTORY
+			// START : GET HOME DIRECTORY
+			const TEST_MESSAGE = {};
+			TEST_MESSAGE["action_url"] = "/directory/home";
+			TEST_MESSAGE["http_type"] = "GET";
+			TEST_MESSAGE["unique_query_id"] = "12345";
+			socket.send(JSON.stringify(TEST_MESSAGE));
+			console.log("Sending GET message");
+			// END : GET HOME DIRECTORY
 
 			// // START : GET HOME DIRECTORY IN INTERVAL
 			// function intervalFunc() {
