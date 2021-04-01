5c5b43e8389358f5533cbbf75970075b563dec80
Manjot Singh <msingh795617@gmail.com>
2021-03-31 22:04:45 -0700

    inital design of profile page

--- app/src/pages/profile.js
+++ app/src/pages/profile.js
@@ -1,9 +1,49 @@
 import React from "react";
+import Avatar from "react-avatar";
+
 
 const Profile = () => {
   return (
     <main>
-      <h1>Hello World!</h1>
+      {/* <h1>Hello World!</h1> */}
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Avatar name="Manjot Singh" round />
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1>Hello, Manjot Singh!</h1>
+      </div>
+
+      <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      >
+        <h1>
+          You're overall Status is: 100%!
+        </h1>
+      </div>
+
+
+
+
+
+
     </main>
   );
 };
