1e6fa25f3a9b4711120f6aae32f3f36c213d1f5d
Adrian Darian <adarian@ucmerced.edu>
2021-04-22 23:41:26 -0700

    fix: modbus class to work with promises

--- api/config/modbus.js
+++ api/config/modbus.js
@@ -6,130 +6,112 @@ const { env, modbus_port } = require("./variables");
 /**
  * This is a simple Modbus overlay that allows us to create multiple instances of the client and open multiple streams of data
  */
-class Modbus {
-    constructor() {
-        this.port = modbus_port;
-        this.client = new ModbusRTU();
-        this.environment = env;
-    }
+ class Modbus {
+	constructor(options) {
+		this.port = options.port || modbus_port;
+		this.client = new ModbusRTU();
+		this.environment = options.environment || env;
+	}
 
-    /**
-     * Sleep is a function that starts a timer on a given ms time
-     * @param {number} ms
-     * @returns {Promise}
-     */
-    sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+	/**
+	 * Sleep is a function that starts a timer on a given ms time
+	 * @param {number} ms
+	 * @returns {Promise}
+	 */
+	sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-    /**
-     * Sets up a new connection on a new device and readies the client for further instructions
-     * @param {number} id - defines which device to read from
-     */
-    connect = async (id) => {
-        this.client
-            .connectTCP(this.environment, { port: this.port })
-            .then(() =>
-                console.info(
-                    `Meter Device id: ${id} . . . TCP Protocol Connected on port ${this.port}`
-                )
-            )
-            .catch((error) => console.error(error.message));
-        this.client.setID(id);
+	/**
+	 * Sets up a new connection on a new device and readies the client for further instructions
+	 * @param {number} id - defines which device to read from
+     * @return {object} {status, message}
+	 */
+	connect = (id) =>
+		new Promise(async (resolve, reject) => {
+			await this.client
+				.connectTCP(this.environment, { port: this.port })
+				.catch((error) => {
+					reject({ status: "rejected", message: error.message });
+				});
+			this.client.setID(id);
 
-        await this.sleep(1000);
-    };
+			await this.sleep(1000);
 
-    /**
-     * Read the Holding Registry data from a given port
-     * @param {number} port - port on the device
-     * @return {float} serial data buffer
-     */
-    read = async (port) => {
-        try {
-            // wait before making request due to promise
-            return await this.client.readHoldingRegisters(
-                `${port}`,
-                2,
-                (error, data) => {
-                    if (error)
-                        console.error(
-                            `Error reading holding register: ${
-                                JSON.stringify(error)
-                            }`
-                        );
-                    else {
-                        console.info(
-                            `Meter device id: ${this.client.getID()}, reading buffer: ${data.buffer.readFloatBE()} on serial port: ${port}`
-                        );
-                        return data.buffer.readFloatBE();
-                    }
-                }
-            );
-        } catch (error) {
-            console.error(`Error reading holding register: ${error}`);
-        }
-    };
+			resolve({
+				status: "connected",
+				message: `Meter Device id: ${id} . . . TCP Protocol Connected on port ${
+					this.port
+				} -- ${new Date().toLocaleString()}`,
+			});
+		}).catch((error) => error);
 
-    /**
-     * Read ALL ports on a given device at once
-     */
-    readAll = async () => {
-        let bufferData = {};
+	/**
+	 * Read the Holding Registry data from a given port
+	 * @param {number} port - port on the device
+	 * @return {float} serial data buffer
+	 */
+	read = (port) =>
+		new Promise((resolve, reject) =>
+			this.client.readHoldingRegisters(`${port}`, 2, (error, data) => {
+				if (error)
+					reject(
+						`Error reading holding register: ${JSON.stringify(
+							error
+						)}`
+					);
 
-        try {
-            await Object.keys(meterdata)?.map(async (meter) => {
-                // wait before making request due to promise
-                await this.client.readHoldingRegisters(
-                    `${meterdata[meter][0]}`,
-                    2,
-                    (error, data) => {
-                        if (error)
-                            console.error(
-                                `Error reading holding register: ${JSON.stringify(
-                                    error
-                                )}`
-                            );
-                        else {
-                            console.info(
-                                `Meter device id: ${this.client.getID()}, reading buffer: ${data.buffer.readFloatBE()} on serial port: ${port}`
-                            );
-                        }
-                        bufferData[meter] = { port, buffer };
-                    }
-                );
-            });
-        } catch (error) {
-            console.error(`Error reading holding register: ${error}`);
-        }
+				// console.info(
+				// 	`Meter device id: ${this.client.getID()}, reading buffer: ${data.buffer.readFloatBE()} on serial port: ${port}`
+				// );
+				resolve(data.buffer.readFloatBE());
+			})
+		);
 
-        return bufferData;
-    };
+	/**
+	 * Read ALL ports on a given device at once
+	 * @return {array<object>} serial data buffer for each port
+	 */
+	readAll = () =>
+		new Promise(async (resolve) => {
+			let bufferData = {};
+			console.info(
+				`Holding Registers for meter device id: ${this.client.getID()}`
+			);
+			await Promise.all(
+				Object.keys(meterdata).map(async (meter) => {
+					const response = await this.read(meterdata[meter][0]);
+					bufferData[meter] = await {
+						port: meterdata[meter][0],
+						buffer: response,
+					};
+				})
+			);
 
-    /**
-     * Open a stream reading holding registry data every 1000ms
-     * @param {number} port - optional
-     * @return {Object<string: Object<number, number>>}
-     */
-    stream = async (port = 1) => {
-        let bufferData = [];
+			resolve(bufferData);
+		});
 
-        try {
-            // output value to console
-            if (port) bufferData.push(await this.readAll());
-            else bufferData.push(await this.read(port));
-            // wait 1000ms before get another device
-            await this.sleep(1000);
-        } catch (error) {
-            // if error, handle them here (it should not)
-            console.error(error);
-        } finally {
-            // after get all data from salve repeat it again
-            setImmediate(() => {
-                this.stream();
-            });
-        }
-
-        return bufferData;
-    };
+	/**
+	 * Open a stream reading holding registry data every 1000ms
+	 */
+	stream = async (running = 1000) => {
+		console.info(
+			`Reading all holding registers. Total time elapsed ${running}ms.`
+		);
+		try {
+			// output value to console
+			const response = await this.readAll();
+			console.table(response);
+			// wait 1000ms before get another device
+			await this.sleep(1000);
+		} catch (error) {
+			// if error, handle them here (it should not)
+			console.error(error);
+		} finally {
+			// after get all data from salve repeat it again
+			setImmediate(() => {
+				this.stream(running + 1000);
+			});
+		}
+	};
 }
 
 module.exports = Modbus;
